66a78ad6fff4b18193f0d585e66190ead8ad08eb
Daria <vichkunina>
2018-04-02 02:30:58 +0500

    fix all + tests

--- server/app/tools/href.js
+++ server/app/tools/href.js
@@ -1,35 +1,30 @@
 'use strict';
-/* eslint-disable no-shadow*/
-
-const message = require('./currentMessage');
-let mes = '**tut** _budet_ message https://github.com/urfu-2017 http://mail.ru/ fdddddd';
-message.getMessage(mes);
-let currentMessage = message.show();
-let hrefArray = [];
-
+/* eslint-disable no-useless-escape */
 const findHref = (currentMessage) => {
-    let linkData = currentMessage.match(/(http|https|ftp):\/\/[^\s]+/i);
+    let linkData =
+    currentMessage.match(/(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})\/([\w\.-]*)/);
     if (!linkData) {
 
         return 0;
     }
 
     const link = linkData[0];
-    const index = linkData.index;
 
-    return { link, index };
+    return { link };
 };
 
-const getHrefArray = (currentMessage) => {
+const getHrefArray = (currentMessage, hrefArray) => {
+    hrefArray = hrefArray || [];
     let link = findHref(currentMessage);
-    if (link.link && link.index + link.link.length - 1 < currentMessage.length) {
+    if (link.link) {
         hrefArray.push(link.link);
-        let currentMessageSlice = currentMessage.slice(link.index + link.link.length);
-        getHrefArray(currentMessageSlice);
+        let current = currentMessage.split(link.link).join('');
+        getHrefArray(current, hrefArray);
     }
+
+    return hrefArray;
 };
 
-getHrefArray(currentMessage);
 exports.getHrefArray = getHrefArray;
 
 
