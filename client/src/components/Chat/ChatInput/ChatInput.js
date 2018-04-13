5607e76d9b867506ee9cf8e3f11562d53811e1d1
Daria <vichkunina>
2018-04-13 12:49:16 +0500

    input правки

--- client/src/components/Chat/ChatInput/ChatInput.js
+++ client/src/components/Chat/ChatInput/ChatInput.js
@@ -1,14 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './ChatInput.css';
 
+class ChatInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { chatInput: '' };
+        this.submitHandler = this.submitHandler.bind(this);
+        this.textChangeHandler = this.textChangeHandler.bind(this);
+    }
 
-export default class Input extends Component {
+    submitHandler(event) {
+        event.preventDefault();
+        this.setState({ chatInput: '' });
+        this.onSend(this.state.chatInput);
+    }
+
+    textChangeHandler(event) {
+        this.setState({ chatInput: event.target.value });
+    }
+
     render() {
         return (
-            <form className= {styles.Wrapper}>
-                <input className={styles.Input} type="text" placeholder="Напишите сообщение..." />
-                <input type = "image" value = "Send" className={styles.Send}/>
+            <form className={styles.Wrapper} onSubmit={this.submitHandler}>
+                <input type="text" className={styles.Input}
+                    onChange={this.textChangeHandler}
+                    value={this.state.chatInput}
+                    placeholder="Write a message..."
+                />
+                <input type = "submit" value = "Send" className={styles.Send}/>
             </form>
         );
     }
 }
+
+ChatInput.defaultProps = {
+};
+
+export default ChatInput;
+
