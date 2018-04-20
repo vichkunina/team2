2babf1c18d50f92851e4389b5a869d3dc49c1e00
Daria <vichkunina>
2018-04-20 18:50:05 +0500

    btn-add-img

--- client/src/components/Chat/ChatInput/index.js
+++ client/src/components/Chat/ChatInput/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-invalid-this*/
 import React from 'react';
 import { PropTypes } from 'mobx-react';
 import { observer, inject } from 'mobx-react';
@@ -7,6 +8,8 @@ import styles from './index.css';
 export default class ChatInput extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { chatInput: '',
+            selectedFile: null };
     }
     static propTypes = {
         chatInputState: PropTypes.observableObject
@@ -17,8 +20,16 @@ export default class ChatInput extends React.Component {
         event.preventDefault();
     }
 
-    changeHandler(event) {
-        this.props.chatInputState.change(event.target.value);
+    fileChangedHandler = (event) => {
+        this.setState({ selectedFile: event.target.files[0] });
+    }
+
+    uploadHandler = () => {
+        console.log('Some useful info for future', this.state.selectedFile);
+    }
+
+    textChangeHandler(event) {
+        this.setState({ chatInput: event.target.value });
     }
 
     render() {
@@ -36,9 +47,17 @@ export default class ChatInput extends React.Component {
                 <button type="button" className={`${styles.EmojiButton} ${styles.Button}`}>
                     <i className="material-icons">tag_faces</i>
                 </button>
+                <div className = {`${styles.AddButton} ${styles.Button}`}>
+                    <label>
+                        <input type="file" onChange={this.fileChangedHandler.bind(this)}
+                            accept="image/*" multiple className={styles.Upload} />
+                        <i className="material-icons">add</i>
+                    </label>
+                </div>
                 <button form="send-message-form" type="submit"
                     className={`${styles.SendButton} ${styles.Button}`}>
-                    <i className="material-icons">send</i>
+                    <i onClick={this.uploadHandler.bind(this)}
+                        className="material-icons">send</i>
                 </button>
             </form>
         );
