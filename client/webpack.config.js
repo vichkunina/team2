b183395d03a9c708fbbe4cd6a4f1359c6acf43ec
Yui Hirasawa <bosnianrainbow@gmail.com>
2018-05-02 19:01:58 +0500

    Фикс загрузки шрифтов в Webpack (#117)
    Фикс загрузки шрифтов в webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -86,6 +86,16 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
             }
         ]
     },
